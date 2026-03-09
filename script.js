e4a58c2ccf47477e1475a2cf127865fd5c59a47b
Kagnos <kagcodes@gmail.com>
2026-03-09 12:25:25 -0500

    Player turns and variable changes Remove score variable Create player objects Rename some variables Create event listeners for board buttons Implement board functionality with turns Style board buttons to center markers

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function runGame () {
     let activePlayer;
     let passivePlayer;
-    let runningScore = { 
-        playerXScore: 0,
-        playerOScore: 0
+    let playerX = {
+        name: "Player X",
+        marker: "X",
+        score: 0
+    };
+    let playerO = {
+        name: "Player O",
+        marker: "O",
+        score: 0
     };
     const allButtons = document.querySelectorAll("button");
-    const playerXName = document.querySelector("#player-x-name");
-    const playerOName = document.querySelector("#player-o-name");
-    const playerX = document.querySelector("#player-x");
-    const playerO = document.querySelector("#player-o");
-    const XMarker = document.querySelector("x-marker");
-    const OMarker = document.querySelector("o-marker");
-    const turnIndicator = document.querySelector("#turn-indicator");
+    const playerXNameInput = document.querySelector("#player-x-name");
+    const playerONameInput = document.querySelector("#player-o-name");
+    const playerXVsText = document.querySelector("#player-x-vs-text");
+    const playerOVsText = document.querySelector("#player-o-vs-text");
+    const turnIndicatorText = document.querySelector("#turn-indicator-text");
 
     allButtons.forEach((button) => 
         button.addEventListener("click", () => {
@@ -23,34 +27,78 @@ function runGame () {
                     return;
                 case "play-again-button":
                     return;
+                case "board-button-1":
+                    return checkActivePlayer(button);
+                case "board-button-2":
+                    return checkActivePlayer(button);
+                case "board-button-3":
+                    return checkActivePlayer(button);
+                case "board-button-4":
+                    return checkActivePlayer(button);
+                case "board-button-5":
+                    return checkActivePlayer(button);
+                case "board-button-6":
+                    return checkActivePlayer(button);
+                case "board-button-7":
+                    return checkActivePlayer(button);
+                case "board-button-8":
+                    return checkActivePlayer(button);
+                case "board-button-9":
+                    return checkActivePlayer(button);
             };
         })
     );
 
-
-
     function startGame (button) {
         button.classList.add("hidden");
-        playerXName.classList.add("hidden");
-        playerOName.classList.add("hidden");
+        playerXNameInput.classList.add("hidden");
+        playerONameInput.classList.add("hidden");
 
-        activePlayer = playerXName.value;
-        passivePlayer = playerOName.value;
+        activePlayer = playerX;
+        passivePlayer = playerO;
 
-        if (playerXName.value !== "") playerX.textContent = activePlayer;
-        if (playerOName.value !== "") playerO.textContent = passivePlayer;
+        if (playerXNameInput.value !== "") {
+            playerX.name = playerXNameInput.value;
+            playerXVsText.textContent = playerX.name;
+        };
+        if (playerONameInput.value !== "") {
+            playerO.name = playerONameInput.value;
+            playerOVsText.textContent = playerO.name;
+        };
 
-        turnIndicator.textContent = `${activePlayer}'s turn`;
+        turnIndicatorText.textContent = `${activePlayer.name}'s turn`;
 
-        playerX.classList.remove("hidden");
-        playerO.classList.remove("hidden");
-        turnIndicator.classList.remove("hidden");
+        playerXVsText.classList.remove("hidden");
+        playerOVsText.classList.remove("hidden");
+        turnIndicatorText.classList.remove("hidden");
     };
 
-    function changeTurns (activePlayer, passivePlayer) {
-        activePlayer = passivePlayer;
-        passivePlayer = activePlayer;
-        turnIndicator.textContent = `${activePlayer}'s turn`;
+    function checkActivePlayer (button) {
+        if (activePlayer === playerX && button.textContent === "") {
+            let xMarker = document.querySelector(".x-marker");
+            xMarker = xMarker.cloneNode(true);
+            button.appendChild(xMarker);
+
+            changeTurns (playerX, playerO);
+            checkGameOver();
+        } else if (activePlayer === playerO && button.textContent === "") {
+            let oMarker = document.querySelector(".o-marker");
+            oMarker = oMarker.cloneNode(true);
+            button.appendChild(oMarker);
+            
+            changeTurns (playerO, playerX);
+            checkGameOver();
+        } else return;
+    };
+
+    function changeTurns (player1, player2) {
+        passivePlayer = player1;
+        activePlayer = player2;
+        turnIndicatorText.textContent = `${activePlayer.name}'s turn`;
+    };
+
+    function checkGameOver () {
+        return;
     };
 
 };
