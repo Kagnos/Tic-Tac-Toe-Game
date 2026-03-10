f7d8c7b7448ed0b4483406548b912d50ab565e11
Kagnos <kagcodes@gmail.com>
2026-03-10 08:27:28 -0500

    Win conditions and game over consequences Fix JavaScript lines that check if tile has been played in before Create scoreboard and show it when start game button is pressed Create game info container and style to center text Create check for tie scenario and specific win scenario Create consequences for game over Style markers slightly bigger Style button container gap Create hard variables for each header button

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function runGame () {
     let activePlayer;
-    let passivePlayer;
     let playerX = {
         name: "Player X",
         marker: "X",
@@ -12,50 +11,39 @@ function runGame () {
         score: 0
     };
     const allButtons = document.querySelectorAll("button");
+    const startGameButton = document.querySelector("#start-game-button");
+    const newGameButton = document.querySelector("#new-game-button");
+    const playAgainButton = document.querySelector("#play-again-button");
     const playerXNameInput = document.querySelector("#player-x-name");
     const playerONameInput = document.querySelector("#player-o-name");
     const playerXVsText = document.querySelector("#player-x-vs-text");
     const playerOVsText = document.querySelector("#player-o-vs-text");
-    const turnIndicatorText = document.querySelector("#turn-indicator-text");
+    const scoreboard = document.querySelector("#scoreboard");
+    const gameMessage = document.querySelector("#game-message");
+    let xSVG = document.querySelector("#x-svg");
+    let oSVG = document.querySelector("#o-svg");
 
     allButtons.forEach((button) => 
         button.addEventListener("click", () => {
-            switch(button.id) {
+            if (button.id.includes("board-button")) {
+                return checkActivePlayer(button);
+            } else switch(button.id) {
                 case "start-game-button":
-                    return startGame(button);  
+                    return startGame();  
                 case "new-game-button":
-                    return;
+                    return startNewGame();
                 case "play-again-button":
-                    return;
-                case "board-button-1":
-                    return checkActivePlayer(button);
-                case "board-button-2":
-                    return checkActivePlayer(button);
-                case "board-button-3":
-                    return checkActivePlayer(button);
-                case "board-button-4":
-                    return checkActivePlayer(button);
-                case "board-button-5":
-                    return checkActivePlayer(button);
-                case "board-button-6":
-                    return checkActivePlayer(button);
-                case "board-button-7":
-                    return checkActivePlayer(button);
-                case "board-button-8":
-                    return checkActivePlayer(button);
-                case "board-button-9":
-                    return checkActivePlayer(button);
+                    return playGameAgain();
             };
         })
     );
 
-    function startGame (button) {
-        button.classList.add("hidden");
+    function startGame () {
+        startGameButton.classList.add("hidden");
         playerXNameInput.classList.add("hidden");
         playerONameInput.classList.add("hidden");
 
         activePlayer = playerX;
-        passivePlayer = playerO;
 
         if (playerXNameInput.value !== "") {
             playerX.name = playerXNameInput.value;
@@ -66,41 +54,84 @@ function runGame () {
             playerOVsText.textContent = playerO.name;
         };
 
-        turnIndicatorText.textContent = `${activePlayer.name}'s turn`;
+        gameMessage.textContent = `${activePlayer.name}'s turn`;
 
         playerXVsText.classList.remove("hidden");
         playerOVsText.classList.remove("hidden");
-        turnIndicatorText.classList.remove("hidden");
+        scoreboard.classList.remove("hidden");
+        gameMessage.classList.remove("hidden");
     };
 
     function checkActivePlayer (button) {
-        if (activePlayer === playerX && button.textContent === "") {
-            let xMarker = document.querySelector(".x-marker");
-            xMarker = xMarker.cloneNode(true);
+        if (activePlayer === playerX && button.childNodes.length === 0) {
+            let xMarker = xSVG.cloneNode(true);
+            xMarker.classList.remove("header-svg");
+            xMarker.classList.add("x-marker");
             button.appendChild(xMarker);
-
-            changeTurns (playerX, playerO);
-            checkGameOver();
-        } else if (activePlayer === playerO && button.textContent === "") {
-            let oMarker = document.querySelector(".o-marker");
-            oMarker = oMarker.cloneNode(true);
+            checkGameOver(playerX);
+        } else if (activePlayer === playerO && button.childNodes.length === 0) {
+            let oMarker = oSVG.cloneNode(true);
+            oMarker.classList.remove("header-svg");
+            oMarker.classList.add("o-marker");
             button.appendChild(oMarker);
-            
-            changeTurns (playerO, playerX);
-            checkGameOver();
+            checkGameOver(playerO);
         } else return;
     };
 
-    function changeTurns (player1, player2) {
-        passivePlayer = player1;
-        activePlayer = player2;
-        turnIndicatorText.textContent = `${activePlayer.name}'s turn`;
+    function checkGameOver (currentPlayer) {
+        const allXMarkers = document.querySelectorAll(".x-marker");
+        const allOMarkers = document.querySelectorAll(".o-marker");
+
+        if (allXMarkers.length + allOMarkers.length === 9) {
+            gameMessage.textContent = "Cat's game";
+            newGameButton.classList.remove("hidden");
+            playAgainButton.classList.remove("hidden");
+        } else checkWinCondition(currentPlayer, allXMarkers, allOMarkers);
     };
 
-    function checkGameOver () {
-        return;
+    function checkWinCondition (currentPlayer, allXMarkers, allOMarkers) { 
+        if (currentPlayer === playerX) { 
+            const xTilesArray = [];
+            allXMarkers.forEach((xMarker) => {
+                xTilesArray.push(xMarker.closest(".board-button"));
+            })
+            if (xTilesArray[0] === document.querySelector("#board-button-1") && xTilesArray[1] === document.querySelector("#board-button-2") && xTilesArray[2] === document.querySelector("#board-button-3")) {
+                activePlayer = undefined;
+                playerX.score++;
+                scoreboard.textContent = `${playerX.score} - ${playerO.score}`;
+                gameMessage.textContent = `${playerX.name} wins!`;
+                newGameButton.classList.remove("hidden");
+                playAgainButton.classList.remove("hidden");
+            } else changeTurns (playerO);
+
+        } else if (currentPlayer === playerO) {
+            const oTilesArray = [];
+            allOMarkers.forEach((oMarker) => {
+                oTilesArray.push(oMarker.closest(".board-button"));
+            })
+            if (oTilesArray[0] === document.querySelector("#board-button-1") && oTilesArray[1] === document.querySelector("#board-button-2") && oTilesArray[2] === document.querySelector("#board-button-3")) {
+                activePlayer = undefined;
+                playerO.score++;
+                scoreboard.textContent = `${playerX.score} - ${playerO.score}`;
+                gameMessage.textContent = `${playerO.name} wins!`;
+                newGameButton.classList.remove("hidden");
+                playAgainButton.classList.remove("hidden");
+            } else changeTurns (playerX);
+        };
+    };
+
+    function changeTurns (nextPlayer) {
+        activePlayer = nextPlayer;
+        gameMessage.textContent = `${activePlayer.name}'s turn`;
     };
 
 };
 
 runGame();
+
+
+// trying to figure out how to write out the win condition checks more efficiently
+// and trying to figure out how to make the code the same for both players - I don't think this will happen though
+// finish writing win conditions
+
+// Git message:
